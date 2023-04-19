9814682e2c1250ac9f5cf987df939dd71ec9a88d
Viktor Ohad <viktor.ohad@proton.me>
2023-04-19 19:27:36 +0200

    correctly specify harpoon dependency

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,14 +1,5 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
 -- See the kickstart.nvim README for more information
 return {
-	'ThePrimeagen/harpoon'
+  { 'ThePrimeagen/harpoon', version = "*", dependencies= { 'nvim-lua/plenary.nvim' } }
 }
 
--- require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
-
-  -- Git related plugins
-  -- 'tpope/vim-fugitive',
-  -- 'tpope/vim-rhubarb',
