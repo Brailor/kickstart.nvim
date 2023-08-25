d05b32a8d41a7d3c9646cf163baf794cfe0daeaf
Viktor Ohad <viktor.ohad@proton.me>
2023-08-25 17:38:02 +0200

    wip: add java lsp

--- init.lua
+++ init.lua
@@ -307,7 +307,7 @@ vim.keymap.set('n', '<leader>a', '<cmd>:lua require("harpoon.mark").add_file()<c
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'javascript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go','java', 'lua', 'python', 'rust', 'tsx', 'typescript', 'javascript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
@@ -444,9 +444,121 @@ local function get_python_path()
   return active_env_path
 end
 
-print(get_python_path())
+
+function setup_java()
+  local home = os.getenv('HOME')
+
+  -- File types that signify a Java project's root directory. This will be
+  -- used by eclipse to determine what constitutes a workspace
+  local root_markers = {'gradlew', 'mvnw', '.git'}
+  -- local root_dir = require('jdtls.setup').find_root(root_markers)
+
+  -- eclipse.jdt.ls stores project specific data within a folder. If you are working
+  -- with multiple different projects, each project must use a dedicated data directory.
+  -- This variable is used to configure eclipse to use the directory name of the
+  -- current project found using the root_marker as the folder for project specific data.
+  -- local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+  local java_opts = {
+        format = {
+          settings = {
+            -- Use Google Java style guidelines for formatting
+            -- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
+            -- and place it in the ~/.local/share/eclipse directory
+            url = "/.local/share/eclipse/eclipse-java-google-style.xml",
+            profile = "GoogleStyle",
+          },
+        },
+        signatureHelp = { enabled = true },
+        contentProvider = { preferred = 'fernflower' },  -- Use fernflower to decompile library code
+        -- Specify any completion options
+        completion = {
+          favoriteStaticMembers = {
+            "org.hamcrest.MatcherAssert.assertThat",
+            "org.hamcrest.Matchers.*",
+            "org.hamcrest.CoreMatchers.*",
+            "org.junit.jupiter.api.Assertions.*",
+            "java.util.Objects.requireNonNull",
+            "java.util.Objects.requireNonNullElse",
+            "org.mockito.Mockito.*"
+          },
+          filteredTypes = {
+            "com.sun.*",
+            "io.micrometer.shaded.*",
+            "java.awt.*",
+            "jdk.*", "sun.*",
+          },
+        },
+        -- Specify any options for organizing imports
+        sources = {
+          organizeImports = {
+            starThreshold = 9999;
+            staticStarThreshold = 9999;
+          },
+        },
+        -- How code generation should act
+        codeGeneration = {
+          toString = {
+            template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+          },
+          hashCodeEquals = {
+            useJava7Objects = true,
+          },
+          useBlocks = true,
+        },
+        -- If you are developing in projects with different Java versions, you need
+        -- to tell eclipse.jdt.ls to use the location of the JDK for your Java version
+        -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+        -- And search for `interface RuntimeOption`
+        -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+        configuration = {
+          runtimes = {
+            {
+              name = "JavaSE-17",
+              path = home .. "/.sdkman/candidates/java/17.0.8-amzn"
+            },
+          }
+        },
+    -- cmd is the command that starts the language server. Whatever is placed
+    -- here is what is passed to the command line to execute jdtls.
+    -- Note that eclipse.jdt.ls must be started with a Java version of 17 or higher
+    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+    -- for the full list of options
+    cmd = {
+      home .. "/.sdkman/candidates/java/17.0.8-amzn/bin/java",
+      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+      '-Dosgi.bundles.defaultStartLevel=4',
+      '-Declipse.product=org.eclipse.jdt.ls.core.product',
+      '-Dlog.protocol=true',
+      '-Dlog.level=ALL',
+      '-Xmx4g',
+      '--add-modules=ALL-SYSTEM',
+      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+      -- If you use lombok, download the lombok jar and place it in ~/.local/share/eclipse
+      -- '-JAVAAGENT:' .. HOME .. '/.LOCAL/SHARE/ECLIPSE/LOMBOK.JAR',
+
+      -- THE JAR FILE IS LOCATED WHERE JDTLS WAS INSTALLED. THIS WILL NEED TO BE UPDATED
+      -- TO THE LOCATION WHERE YOU INSTALLED JDTLS
+      -- .LOCAL/SHARE/NVIM/MASON/PACKAGES/JDTLS/FEATURES/ORG.ECLIPSE.EQUINOX.EXECUTABLE_3.8.2200.V20230717-2134.JAR
+      '-jar', vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+
+      -- THE CONFIGURATION FOR JDTLS IS ALSO PLACED WHERE JDTLS WAS INSTALLED. THIS WILL
+      -- NEED TO BE UPDATED DEPENDING ON YOUR ENVIRONMENT
+      '-configuration', home .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
+
+      -- USE THE WORKSPACE_FOLDER DEFINED ABOVE TO STORE DATA FOR THIS PROJECT
+      -- '-data', WORKSPACE_FOLDER,
+    }
+  }
+
+  return java_opts
+end
+
 local servers = {
   -- clangd = {},
+  jdtls = {
+    java = setup_java(),
+  },
   gopls = {},
   pyright = {
     python = {
